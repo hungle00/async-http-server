4586ea94b91654a06b385526b3fe3eae3cfb2d21
hungle00 <hungle2048@gmail.com>
2024-10-20 10:05:35 +0700

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# http-server
+
+Build Ruby Http Server from scratch
---
+++ server.rb
@@ -0,0 +1,38 @@
+require 'socket'
+require './http_utils/request_parser'
+require './http_utils/http_responder'
+
+class SimpleServer
+  PORT = ENV.fetch('PORT', 3000)
+  HOST = ENV.fetch('HOST', '127.0.0.1').freeze
+  # number of incoming connections to keep in a buffer
+  SOCKET_READ_BACKLOG = ENV.fetch('TCP_BACKLOG', 12).to_i
+
+  attr_accessor :app
+
+  def initialize(app)
+    self.app = app
+  end
+
+  def start
+    socket = TCPServer.new(HOST, PORT)
+    socket.listen(SOCKET_READ_BACKLOG)
+
+    loop do
+      conn = socket.accept # wait for a client to connect
+      request = RequestParser.call(conn)
+      status, headers, body = app.call(request)
+      HttpResponder.call(conn, status, headers, body)
+    rescue => e
+      puts e.message
+    ensure
+      conn&.close
+    end
+  end
+end
+
+app = Proc.new do
+  ['200', {'Content-Type' => 'text/html'}, ["Hello world! The time is #{Time.now}"]]
+end
+
+SimpleServer.new(app).start
